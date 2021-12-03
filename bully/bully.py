7f95b0fb53ed473659bc83c13dbcb77ef3474b71
donatop4d <juanpablo.donato@balloon-group.com>
2021-12-02 22:37:33 -0300

    Add is_in_election Condvar + Getters and Setters

--- bully/bully.py
+++ bully/bully.py
@@ -10,7 +10,9 @@ class Bully:
         self.conn_manager = connection_manager
         self.peer_hostnames = peer_hostnames
         self.threads = []
+
         self.is_in_election = False
+        self.is_in_election_cv = Condition(Lock())
 
         self.received_ok = False
         self.received_ok_cv = Condition(Lock())
@@ -30,23 +32,55 @@ class Bully:
         # ping_thread.start()
         # self.threads.append(ping_thread)
 
-    def poll_leader(self):
-        while True:
-            if not self.is_leader and not self.is_in_election:
-                self.conn_manager.send_to(self.leader_addr, 'PING')
+    # def poll_leader(self):
+    #     while True:
+    #         if not self.is_leader and not self.is_in_election:
+    #             self.conn_manager.send_to(self.leader_addr, 'PING')
 
-            time.sleep(5)
+    #         time.sleep(5)
+
+    def get_is_in_election(self):
+        self.is_in_election_cv.acquire()
+        is_in_election = self.is_in_election
+        self.is_in_election_cv.release()
+        return is_in_election
+
+    def set_is_in_election(self, value):
+        self.is_in_election_cv.acquire()
+        self.is_in_election = value
+        self.is_in_election_cv.release()
+
+    def get_received_ok(self):
+        self.received_ok_cv.acquire()
+        received_ok = self.received_ok
+        self.received_ok_cv.release()
+        return received_ok
+    
+    def wait_get_received_ok(self, timeout):
+        self.received_ok_cv.acquire()
+        self.received_ok_cv.wait(timeout)
+        received_ok = self.received_ok
+        self.received_ok_cv.release()
+        return received_ok
+
+    def set_received_ok(self, value):
+        self.received_ok_cv.acquire()
+        self.received_ok = value
+        self.received_ok_cv.release()
+
+    def notify_set_received_ok(self, value):
+        self.received_ok_cv.acquire()
+        self.received_ok = True
+        self.received_ok_cv.notify_all()
+        self.received_ok_cv.release()
 
     def begin_election_process(self):
-        if self.is_in_election: return
-        self.is_in_election = True
+        if self.get_is_in_election(): return
+        self.set_is_in_election(True)
 
         self.conn_manager.send_to_mayors('ELECTION')
         #time.sleep(5)
-        self.received_ok_cv.acquire()
-        self.received_ok_cv.wait(5)
-        received_ok = self.received_ok
-        self.received_ok_cv.release()
+        received_ok = self.wait_get_received_ok(5)
 
         if not received_ok:
             self.proclaim_leader()
@@ -66,10 +100,7 @@ class Bully:
 
     def process_ok_message(self, peer_addr):
         print(f'Received OK message from {peer_addr}')
-        self.received_ok_cv.acquire()
-        self.received_ok = True
-        self.received_ok_cv.notify_all()
-        self.received_ok_cv.release()
+        self.notify_set_received_ok(True)
 
     def process_leader_message(self, peer_addr):
         print(f'Received LEADER message from {peer_addr}')
@@ -80,11 +111,8 @@ class Bully:
         self.reset_election_variables()
 
     def reset_election_variables(self):
-        self.is_in_election = False
-
-        self.received_ok_cv.acquire()
-        self.received_ok = False
-        self.received_ok_cv.release()
+        self.set_is_in_election(False)
+        self.set_received_ok(False)
 
 
     def start_receiving_from_peer(self, peer_addr):
